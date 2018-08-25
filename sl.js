12b64a27e188c1b48fa4827dd4f5f01af87fa225
icang69 <36757987+icang69@users.noreply.github.com>
2018-08-24 17:37:22 -0800

    Update sl.js

--- sl.js
+++ sl.js
@@ -38,7 +38,7 @@ j++;}
 
 if(a_to_vh==false)
 
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://mylinkupto.blogspot.com/2018/08/download-link.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://www.download.ikhsan.my.id/p/go-to-url.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 
